30326ea088c88353af3b96ef24d7b1840c1f7137
irfanpule <irfan.pule2@gmail.com>
2023-06-15 10:14:10 +0700

    show menu auth user django

--- djbackoffice/models.py
+++ djbackoffice/models.py
@@ -1,1 +1,15 @@
 # from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from djbackoffice.core import backoffice, BackofficeOptions
+from djbackoffice.settings import DJBACKOFFICE_SHOW_USER_DJANGO
+
+
+class UserBackofficeOpts(BackofficeOptions):
+    list_display = ('username', 'first_name', 'last_name', 'email', 'is_superuser', 'is_staff', 'is_active')
+    search_fields = ('username', 'first_name', 'last_name', 'email',)
+
+
+if DJBACKOFFICE_SHOW_USER_DJANGO:
+    backoffice.register(get_user_model(), UserBackofficeOpts)
+    backoffice.register(Group)
--- djbackoffice/settings.py
+++ djbackoffice/settings.py
@@ -2,3 +2,4 @@ from django.conf import settings
 
 DEBUG = getattr(settings, "DEBUG", False)
 DJANGO_TABLES2_TEMPLATE = getattr(settings, "DJANGO_TABLES2_TEMPLATE", "django_tables2/bootstrap5.html")
+DJBACKOFFICE_SHOW_USER_DJANGO = getattr(settings, "DJBACKOFFICE_SHOW_USER_DJANGO", True)
